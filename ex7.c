d4bd94ed4d7dde3c7297f7bb28c4736ec2ac0f65
Victor Rolim Costa <victorrolimc@gmail.com>
2016-05-10 13:23:14 -0300

    Create ex7.c

---
+++ ex7.c
@@ -0,0 +1,10 @@
+/*Exercicio 7: Automato Finito Determinado
+ * Authors: Victor Rolim Costa
+            Rajiv Machado
+ * 
+ * Profº.: Ruben C. Benante.
+ *
+ * Descricao: 
+ *
+ * This program is protceted by GNU LICENSE v3.0; for more legal information, check the LICENSE file.            
+ * */
